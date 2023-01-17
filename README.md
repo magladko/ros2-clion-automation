7cc08988689478cee54be6f5ed836bbb2d07393b
magladko <kornel.marek@gmail.com>
2023-01-17 22:43:50 +0000

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# ROS2 with Clion
+
+This repository is made for quick setup for the JetBrains Clion [ROS2 setup tutorial](https://www.jetbrains.com/help/clion/ros2-tutorial.html).
+It is prepared for the Ubuntu approach with the [ROS2 Humble](https://docs.ros.org/en/humble/index.html).
+
+Before executing make sure to personalize any paths especially in the 'cmake_commands.sh' file.
+The first compilation must be performed manually:  
+`colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja`
+
+## [ROS2 setup tutorial](https://www.jetbrains.com/help/clion/ros2-tutorial.html)
---
+++ src/cpp_pubsub/cmake_commands.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+colcon build
+
+PACKAGE_SRC_PATH=~/projects/dev_ws/log/latest_build/cpp_pubsub
+PACKAGE_INSTALL_PATH=~/projects/dev_ws/install/cpp_pubsub
+PACKAGE_BUILD_PATH=~/projects/dev_ws/build/cpp_pubsub
+
+cd $PACKAGE_SRC_PATH
+
+CMAKE_PREFIX_PATH=/opt/ros/humble /usr/bin/cmake $PACKAGE_SRC_PATH -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja -DCMAKE_INSTALL_PREFIX=$PACKAGE_INSTALL_PATH
+CMAKE_PREFIX_PATH=/opt/ros/humble /usr/bin/cmake --build $PACKAGE_BUILD_PATH -- -j16 -l16
+CMAKE_PREFIX_PATH=/opt/ros/humble /usr/bin/cmake --install $PACKAGE_BUILD_PATH
+
+cp $PACKAGE_BUILD_PATH/compile_commands.json $PACKAGE_BUILD_PATH/../
