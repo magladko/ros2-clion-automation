e36f3997fa2b835e2b7998de034fc71209c227af
magladko <kornel.marek@gmail.com>
2023-01-18 00:39:24 +0000

    script fix

--- src/cpp_pubsub/cmake_commands.sh
+++ src/cpp_pubsub/cmake_commands.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-colcon build
 
 PACKAGE_SRC_PATH=~/projects/dev_ws/log/latest_build/cpp_pubsub
 PACKAGE_INSTALL_PATH=~/projects/dev_ws/install/cpp_pubsub
 PACKAGE_BUILD_PATH=~/projects/dev_ws/build/cpp_pubsub
-
-cd ~/projects/dev_ws/log/latest_build/cpp_pubsub
 
 CMAKE_PREFIX_PATH=/opt/ros/humble /usr/bin/cmake $PACKAGE_SRC_PATH -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja -DCMAKE_INSTALL_PREFIX=$PACKAGE_INSTALL_PATH
 CMAKE_PREFIX_PATH=/opt/ros/humble /usr/bin/cmake --build $PACKAGE_BUILD_PATH -- -j16 -l16
